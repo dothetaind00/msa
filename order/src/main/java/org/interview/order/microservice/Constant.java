244c2485d6e73cd0d4fc5e57b08c3d2387ca167a
Do The Tai <taidt15@fpt.com>
2025-05-20 23:38:20 +0700

    logicA

--- order/src/main/java/org/interview/order/microservice/Constant.java
+++ order/src/main/java/org/interview/order/microservice/Constant.java
@@ -2,4 +2,5 @@ package org.interview.order.microservice;
 
 public class Constant {
     public static final String HELLO = "Hello";
+    public static final String A = "A";
 }
---
+++ order/src/main/java/org/interview/order/microservice/DoLogicA.java
@@ -0,0 +1,8 @@
+package org.interview.order.microservice;
+
+public class DoLogicA {
+    public static void main(String[] args) {
+        System.out.println("Say " + Constant.HELLO);
+        System.out.println("Do Logic " + Constant.A);
+    }
+}
