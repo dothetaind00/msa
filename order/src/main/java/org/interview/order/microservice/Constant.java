f7b9b311b28481474bb939af11533597fe073c68
Do The Tai <taidt15@fpt.com>
2025-05-21 00:07:49 +0700

    logicB

--- order/src/main/java/org/interview/order/microservice/Constant.java
+++ order/src/main/java/org/interview/order/microservice/Constant.java
@@ -3,4 +3,5 @@ package org.interview.order.microservice;
 public class Constant {
     public static final String HELLO = "Hello";
     public static final String A = "A";
+    public static final String B = "B";
 }
